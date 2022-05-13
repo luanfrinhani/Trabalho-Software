97f34c289a2bb249aa249f4ff66e557b7d84d675
Luan <luan@uppersoft.com.br>
2022-05-13 16:46:34 -0300

    Retira coisas e arquivos desnecessários do tema

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,3 @@
 <div class="kt-footer__copyright">
-    2020&nbsp;&copy;&nbsp;<a href="http://keenthemes.com/metronic" target="_blank" class="kt-link">Keenthemes</a>
-</div>
-<div class="kt-footer__menu">
-    <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">About</a>
-    <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
-    <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
+    2022&nbsp;&copy;&nbsp;<a href="http://keenthemes.com/metronic" target="_blank" class="kt-link">Luanzin dos compuiters</a>
 </div>
