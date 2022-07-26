99846b73f3ec70722e28ef9e197481a48c15f088
Luan <luan@uppersoft.com.br>
2022-07-26 09:35:01 -0300

    Cria tela de editar um pedido e refatora a rota de edit

--- app/Model/System/MaterialUtilizado.php
+++ app/Model/System/MaterialUtilizado.php
@@ -3,8 +3,16 @@
 namespace App\Model\System;
 
 use App\Traits\Core\Uuid;
+use Brick\Math\BigInteger;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+/**
+ * @property string id
+ * @property string material_id
+ * @property string pedido_id
+ * @property BigInteger material_amount
+ * @property BelongsTo material
+ */
 class MaterialUtilizado extends Model
 {
     use Uuid;
@@ -12,4 +20,9 @@ class MaterialUtilizado extends Model
     protected $fillable = [
         'material_id', 'pedido_id', 'material_amount'
     ];
+
+    public function material(): BelongsTo
+    {
+        return $this->belongsTo(Material::class, 'material_id');
+    }
 }
